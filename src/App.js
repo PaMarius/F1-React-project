b5d0187efccfc2af1810c0cbf3c087c472839be0
PaMarius <100645047+PaMarius@users.noreply.github.com>
2022-07-27 16:41:52 +0300

    updateing driverCard and adding style

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import {mockData} from './Constant/drivers.js';
 import { DriverCard } from './Components/driverCard';
 
 function App() {
+  let indexDriver;
+
+  const sortedMockData = mockData.sort((a, b) =>
+  b.points - a.points
+);
 
   return (
+  <>
+    <div className='formula1-title'>Formula 1</div>
     <div className='cards'>
-        {mockData.map((driver, index)=>(
-            <DriverCard key={driver.number} driver={driver} index={index}/> 
+        {sortedMockData.map((driver, index)=>(
+            <DriverCard  key={driver.number} driver={driver} index={index}/> 
         ))}
     </div>
+    </>
   );
 }
 
--- src/Components/driverCard.js
+++ src/Components/driverCard.js
@@ -1,37 +1,47 @@
 import './driverCard.css'; 
 
 export const DriverCard = ({driver, index}) => {
-    const {points, firstName, lastName, team, number, country} = driver
+    const {points, firstName, lastName, team, number, country, image} = driver
 
     return (<div className='driver-card'>
         <div className="driver-stats">
-  <div className="rank">{index + 1}</div>
-  <div className="driver-points">
-  {points} <span className="backg-blk points">PTS</span>
-  </div>
-</div>
-<div className="driver-info">
-  <div className="driver-name">
-    <span className="first-name">{firstName.toUpperCase()}</span>
-    <span className="last-name">{lastName.toUpperCase()}</span>
-  </div>
-  <div className="driver-country">{country}
-    {/* <img className="country-image" alt="" /> */}
-  </div>
-</div>
-<div className="driver-team">{team}</div>
-<div className="driver-appearance">
-  <div  className="driver-number">{number}</div>
-  <div className="driver-photo">
-    <img
-      className="driver-image"
-      src="${image}"
-      alt=""
-    />
-  </div>
-</div>
-<div className="flip-card-back">
-  <p className="info-card">Some Informations</p>
-</div>
-   </div> )
+          <div className="rank">{index + 1}</div>
+          <div className='score'>
+            <div className='add-points'>
+            <div className='add-points-bttn'>
+                <button className='increase-points'></button>
+                <button className='decrease-points'></button>
+            </div>
+            </div>
+            <div className='points'>
+          <div className="driver-points">
+          {points} <span className="backg-blk points-text">PTS</span>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div className="driver-info">
+          <div className="driver-name">
+            <span className="first-name">{firstName.toUpperCase()}</span>
+            <span className="last-name">{lastName.toUpperCase()}</span>
+          </div>
+          <div className="driver-country">
+            <img className="country-image" src={require(`../Constant/country/${country}.jpg`)} alt="" />
+          </div>
+        </div>
+        <div className="driver-team">{team}</div>
+        <div className="driver-appearance">
+          <div  className="driver-number">{number}</div>
+          <div className="driver-photo">
+            <img
+              className="driver-image"
+              src={image}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flip-card-back">
+          <p className="info-card">Some Informations</p>
+        </div>
+      </div> )
 }
